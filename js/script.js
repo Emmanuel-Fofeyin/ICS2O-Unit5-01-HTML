82a8a14d1dcc6d64472a6a64a43028eb3e0b72c7
Emmanuel Israel Jr <99355350+Emmanuel-Fofeyin@users.noreply.github.com>
2022-04-25 16:25:46 +0000

    changed code in style.css and index.html

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2020 Emmanuel-Fofeyin All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Emmanuel.FN
+// Created on: April 22 2020
 // This file contains the JS functions for index.html
 
 /**
@@ -23,4 +23,15 @@ function updateSliderValue(valueFromSlider) {
   document.getElementById("slider-value").innerHTML = valueFromSlider
 }
 
+function random() {
+   // input
+  const sliders value = parseInt(document.getElementById('sliders value').value)
+
+  // process
+  const sliders value = Math.floor(Math.random() * 10) + 1;  // returns a random integer from 1 to 10 
+
+  // output
+  document.getElementById('area').innerHTML = 'The Value is: ' +  value
+}
+}
 /**
