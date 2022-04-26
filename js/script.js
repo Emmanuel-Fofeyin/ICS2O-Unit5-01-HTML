83f8dbb5380fa4982631e1e107f20ad91e26b417
Emmanuel Israel Jr <99355350+Emmanuel-Fofeyin@users.noreply.github.com>
2022-04-26 16:22:21 +0000

    changed code in style.css and script.js

--- js/script.js
+++ js/script.js
@@ -4,34 +4,42 @@
 // Created on: April 22 2020
 // This file contains the JS functions for index.html
 
+"use strict";
+
 /**
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register(
-    "/ICS2O-Unit5-01-HTML-Slider-Example/sw.js",
-    {
-      scope: "/ICS2O-Unit5-01-HTML-Slider-Example/",
-    }
-  )
+  navigator.serviceWorker.register("/ICS2O-Unit-5-01-HTML/sw.js", {
+    scope: "/ICS2O-Unit-5-01-HTML/",
+  });
 }
+
+const randomNumber = Math.floor(Math.random() * 6) + 1;
 
 /**
  * This function updates the slider value.
  */
 function updateSliderValue(valueFromSlider) {
-  document.getElementById("slider-value").innerHTML = valueFromSlider
+  document.getElementById("slider-value").innerHTML = valueFromSlider;
 }
 
-function random() {
-   // input
-  const sliders value = parseInt(document.getElementById('sliders value').value)
+/**
+ * This function compares slider with random number.
+ */
+function guessClicked() {
+  const sliderValue = slider.value;
 
   // process
-  const sliders value = Math.floor(Math.random() * 10) + 1;  // returns a random integer from 1 to 10 
-
-  // output
-  document.getElementById('area').innerHTML = 'The Value is: ' +  value
+  if (sliderValue == randomNumber) {
+    document.getElementById("guess-result").innerHTML =
+      "Answer is " + randomNumber + "!" + " Congratulations!";
+    // console.log("Congratulations!")
+  }
+  //  block of code to be executed if condition1 is true
+  if (sliderValue != randomNumber) {
+    document.getElementById("guess-result").innerHTML =
+      "Answer is " + randomNumber + "!" + " Try again!";
+    // console.log("Try again!")
+  }
 }
-}
-/**
